72b584238a654655c786bbbaddf64fd6704fb6db
somboonnontaganok <somboonnontaganok@gmail.com>
2023-04-25 21:26:47 +0700

    bootstrap config finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Footer from './components/footer'
+import Login from './components/Login';
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Binary Tree Fit</h1>
+      {/* <h1>Binary Tree Fit</h1>
       <p>NavBar</p>
-      <Footer />
+      <Footer /> */}
+      <Login />
     </div>
   )
 }
