2c9652e46cced614484762b70d022b3c1d8d0121
Somboon Nontaganok <125272647+somboonnontaganok@users.noreply.github.com>
2023-04-26 00:30:46 +0700

    home html

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import Footer from './components/footer'
+<<<<<<< HEAD
 import Login from './components/Login';
+=======
+import Navbar from './components/Navbar';
+>>>>>>> 3387630 (add home html)
 
 function App() {
 
   return (
     <div className="App">
+<<<<<<< HEAD
       {/* <h1>Binary Tree Fit</h1>
       <p>NavBar</p>
       <Footer /> */}
       <Login />
+=======
+      <Navbar/>
+      <Footer />
+>>>>>>> 3387630 (add home html)
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,37 @@ import React from 'react';
 
 const Navbar = () => {
     return ( 
-        <ul>
-            <li>Programs</li>
-        </ul>
+        <>
+            <nav class="navbar navbar-expand-lg navbar-light ">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="../src/assets/img/Thumbnail.png"/></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active text-dark" aria-current="page" href="#">Programs</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-warning" href="#">Feature</a>
+                        </li>
+                    </ul>
+                    <ul class="d-flex">
+                        <li class="nav-item">
+                            <button class="btn btn-light" type="submit">Log In</button>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href="#">&nbsp;</a>
+                        </li>
+                        <li class="nav-item">
+                        <button class="btn btn-outline-warning" type="submit">Sign Up</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        </>
      );
 }
  
